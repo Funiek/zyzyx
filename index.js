db14c78b30e299eab06d9799e3a766cf482c2ba2
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2022-05-14 01:35:38 +0200

    Changed endpoints to products from user. Tested connection

--- index.js
+++ index.js
@@ -4,37 +4,49 @@ const port = 3000;
 
 const mongo = require("mongodb");
 const MongoClient = mongo.MongoClient;
-const connectionString = ""
+const connectionString =
+  "mongodb+srv://funiek:<passwd>@cluster0.rqdrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const bodyParser = require("body-parser");
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
-    .then(client => {
-        console.log("Connected to Database");
-        const db = client.db('zyzyx');
-        const userCollection = db.collection('user');
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("zyzyx");
+    const productsCollection = db.collection("products");
 
-        app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-        app.get("/", (req, res) => {
-            res.sendFile(__dirname + "/views/index.html");
-        });
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/views/index.html");
+    });
 
-        app.post("/user", (req, res) => {
-            userCollection.insertOne(req.body)
-                .then(result => {
-                    console.log(result)
-                    res.send("Wyslano");
-                })
-                .catch(error => console.error(error));
+    app.post("/products", (req, res) => {
+      productsCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+          res.send("Wyslano");
+        })
+        .catch((error) => console.error(error));
+    });
 
-        });
+    app.get("/products", (req, res) => {
+      const query = { runtime: { $lt: 15 } };
 
-        app.listen(process.env.PORT || port, () => {
-            console.log(`Started listening on port ${port}`);
-        });
-    })
-    .catch(console.error);
+      const options = {
+        sort: "name",
+        projection: { _id: 0, name: 1, manufacturer: 1, price: 1, currency: 1 },
+      };
 
+      const products = productsCollection.find(query,options);
 
+      console.log(JSON.stringify(products));
+      res.json(products);
+    });
 
+    app.listen(process.env.PORT || port, () => {
+      console.log(`Started listening on port ${port}`);
+    });
+  })
+  .catch(console.error);
